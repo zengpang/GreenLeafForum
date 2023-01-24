31683f189450e7ff493f907b4a4e81dfa153d42f
曾胖 <2516629927@qq.com>
2023-01-24 19:27:32 +0800

    更新Pagination组件，消除组件bug

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -3,7 +3,8 @@
         <button @click="changeBtn">首页</button>
         <button @click="changeBtn">上一页</button>
         <button v-if="jduge" class="pagebtn">......</button>
-        <button v-for="btn in pagebtns" @click="changeBtn(btn)" :class="[{ currentPage: btn == currentPage }, 'pagebtn']">
+        <button v-for="btn in pagebtns" @click="changeBtn(btn)"
+            :class="[{ currentPage: btn == currentPage }, 'pagebtn']">
             {{ btn }}
         </button>
         <button @click="changeBtn">下一页</button>
@@ -24,59 +25,61 @@ export default {
         changeBtn(page) {
             //如果形参page不为number类型则判断按钮为首页按钮，上一页按钮，下一页按钮
             if (typeof page != 'number') {
-                //通过按钮组件文本内容进行判断
-                switch (page.target.innerText) {
-                    case '上一页':
-                        $('button.currentPage').prev().click();
-                        break;
-                    case '下一页':
-                        $('button.currentPage').next().click();
-                        break;
-                    case '首页':
-                        this.pagebtns = [1, 2, 3, 4, 5, '.....'];
-                        this.changeBtn(1);
-                        break;
-                    default:
-                        break;
-                }
-                return;
+                try {
+                    //通过按钮组件文本内容进行判断
+                    switch (page.target.innerText) {
+                        case '上一页':
+                            $('button.currentPage').prev().click();
+                            break;
+                        case '下一页':
+                            $('button.currentPage').next().click();
+                            break;
+                        case '首页':
+                            this.pagebtns = [1, 2, 3, 4, 5, '.....'];
+                            this.changeBtn(1);
+                            break;
+                        default:
+                            break;
+                    }
+                    return;
+                } catch (ex) {
+                    return;
+                } 
             }
-            this.currentPage=page;
+            this.currentPage = page;
             //当页面数量大于4的时候
-            if(page>4)
-            {
+            if (page > 4) {
                 //显示页码省略按钮
-                this.jduge=true;
-            }else
-            {
-                this.jduge=false;
+                this.jduge = true;
+            } else {
+                this.jduge = false;
             }
-            if(page==this.pagebtns[4])
-            {
+            if (page == this.pagebtns[4]) {
                 this.pagebtns.shift();//移除第一个元素
-                this.pagebtns.splice(4,0,this.pagebtns[3]+1);//添加最后一个
-            }else if(page==this.pagebtns[0]&&page!=1)
-            {
+                this.pagebtns.splice(4, 0, this.pagebtns[3] + 1);//添加最后一个
+            } else if (page == this.pagebtns[0] && page != 1) {
                 //先在第一个位置加一个
-                this.pagebtns.unshift(this.pagebtns[0]-1);
+                this.pagebtns.unshift(this.pagebtns[0] - 1);
                 //移除最后一个数字
-                this.pagebtns.splice(5,1);
+                this.pagebtns.splice(5, 1);
             }
-            this.$emit('handleList',this.currentPage);
+            //触发并将当前页码传参给handleList事件，
+            this.$emit('handleList', this.currentPage);
         }
     }
 }
 </script>
 <style scoped>
-  .pageination{
+.pageination {
     margin-top: 5px;
     margin-bottom: 20px;
     background-color: white;
     padding: 6px 20px;
     border-radius: 5px;
     border: 1px solid #888888;
-  }
-  button{
+}
+
+button {
     background-color: #fff;
     border: 1px solid #ddd;
     color: #778087;
@@ -87,16 +90,18 @@ export default {
     padding: 0 2px;
     width: 55px;
     height: 29px;
-  }
-  .pagebtn{
+}
+
+.pagebtn {
     position: relative;
     bottom: 1px;
     width: 40px;
     margin: 0 4px;
-  }
-  .currentPage {
+}
+
+.currentPage {
     color: white;
     background-color: #1f1b1b;
 
-  }
+}
 </style>
